879c308de6fa0c2bf8f9fbda499bbd38cf1938f1
Kriti <kriti.srivastava002@gmail.com>
2025-02-22 23:57:22 +0530

    Some more changes to the python API Part - Analysis of Favourites Bar

--- src/main/java/com/demo/MovieMania/Config/SecurityConfig.java
+++ src/main/java/com/demo/MovieMania/Config/SecurityConfig.java
@@ -52,9 +52,10 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
@@ -62,4 +63,6 @@ public class SecurityConfig {
 
         return source;
     }
+
+
 }
